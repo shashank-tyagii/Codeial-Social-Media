0614cec0bfe33ec7a2ee66825a0c09cba14ce200
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-03-28 00:16:12 +0530

    Setup Express server routing

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// routing all the URLs to route index file i.e App should use this file for any URL
+app.use('/', require('./routes/index'));
+
+// This code is for setting up and connecting to server
 app.listen(port, function(err){
     if (err){
         console.log('Error in running the server : ', err);
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// This file will be used to set all routes on the URL to the respective locations
+
+const express= require ('express');
+
+const router = express.Router();
+
+
+
+
+// Exporting this to be available/use at main index.js file 
+module.exports = router;  
