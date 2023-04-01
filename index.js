ea670071e9200b1d76a2d8174cd2c101e906dc9f
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-04-01 21:52:29 +0530

    Add static assets to each layout

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 // Adding ejs Page layouts. It should be before routes because it will render first layout.ejs
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
+app.set('layout extractStyles', true);   // Extract CSS file to the layout.ejs
+app.set('layout extractScripts', true);  // Extract JS file to the lyout.ejs
+
+// Setup URL Middleware - to process data sent through an HTTP request body.
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({}));                   // Parsing form data from URL - Middleware, not for URL 
 
 // routing all the URLs to route index file i.e App should use this file for any URL
 app.use('/', require('./routes/index'));
@@ -12,6 +18,9 @@ app.use('/', require('./routes/index'));
 //setup view engine
 app.set('view engine' , 'ejs');
 app.set('views' , './views');
+
+// setup all Static files - Images, CSS and JS as a middleware to render
+app.use (express.static('./assets'));                // Middleware to include CSS,JS, Images etc
 
 // This code is for setting up and connecting to server
 app.listen(port, function(err){
