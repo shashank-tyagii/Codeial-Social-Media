ce6ed8be2ae6996ab8bb786e76d5ce180bc11fa1
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-04-01 23:57:23 +0530

    Route and render SignUp and SignIn page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,15 @@ module.exports.profile = function (req,res){             // Module.exports becau
         title: "My profile "
     });
 }
+
+module.exports.signIn = function (req,res){             // Module.exports because we want to send this function to route when home page route is requested
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In "
+    });
+}
+
+module.exports.signUp = function (req,res){             // Module.exports because we want to send this function to route when home page route is requested
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up "
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile' , usersController.profile);
+router.get('/sign-in',  usersController.signIn );
+router.get('/sign-up',  usersController.signUp);
 
 
 // Exporting this to be available/use at main index.js file 
