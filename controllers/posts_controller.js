127d0698eb050d98668b20ccf19c9d1800d9085a
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-04-06 12:49:25 +0530

    Delete a post and associated comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function (req,res){
     Post.create({
@@ -10,4 +11,21 @@ module.exports.create = function (req,res){
         console.log("Error in creating a post");
     })
 
+}
+
+// Delete a post and associated comments
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id).then (function(post){
+        // .id means converting the object id(_id) into string
+        if (post.user == req.user.id)        {               // checking whether the person deleting the post is the same who created it
+            post.remove();
+
+            // Delete all the comments on this post i.e all the comments having post ID as the query ID
+            Comment.deleteMany({post : req.params.id}). then(function(){
+                return res.redirect('back');
+            });
+
+        } 
+        return res.redirect('back');
+    })
 }
