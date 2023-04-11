f63b7395955fb0bcaa02706bf42fb427f89e22b2
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-04-11 13:54:27 +0530

    Create JWT token and Sign-in API

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,28 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+// To get the Sign-In data
+module.exports.createSession = async function (req,res){      
+    try{
+        let user = await User.findOne({ email : req.body.email   });
+
+        if (!user || user.password != req.body.password){
+            return res.status(422).json({
+                message : "Invalid username/password"
+            });
+        }
+
+        return res.status(200).json({
+            message : "Sign in successful. Keep your token safe",
+            data : {
+                token : jwt.sign(user.toJSON(), 'codeial', {expiresIn : 10000})
+            }
+        })
+
+    } catch (err){
+        console.log('Error in JWT Auth', err);
+        return res.status(500).json({
+            messgae : 'Internal server errror'
+        })
+    }
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,6 +2,6 @@ const express= require ('express');
 const router = express.Router();
 
 router.use('/posts', require('./posts'));  // direct to posts.js file 
-
+router.use('/users', require('./users'));  // direct to posts.js file 
 
 module.exports = router;  
