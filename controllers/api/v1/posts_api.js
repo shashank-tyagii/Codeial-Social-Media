46b1e76a4616b229f61dd82fd000e6e8c9cba551
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-04-11 12:53:59 +0530

    Delete API without Authentication

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,45 @@
-module.exports.index = function(req,res){
-    return res.json(200, {
+const Post = require('../../../models/post');
+const Comment= require('../../../models/comment');
+
+module.exports.index = async function(req,res){
+
+      // To get all the posts on Home page
+      let posts = await Post.find().populate('user')      // Populate - This function will replace the user ID with user document in the posts
+      .sort('-createdAt')
+      .populate({                                        // Populate all users from comment array
+          path: 'comments',
+          populate: {
+              path: 'user'
+          } });
+
+
+    return res.status(200).json( {
         message : "List of posts",
-        posts : [],
+        posts : posts,
     })
+};
+
+
+
+// Delete a post and associated comments
+module.exports.destroy = async function(req, res){
+    try {
+        let post = await Post.findById(req.params.id);
+        
+        post.deleteOne();
+    
+        // Delete all the comments on this post i.e all the comments having post ID as the query ID
+        await Comment.deleteMany({post : req.params.id});
+
+        return res.status(200).json({
+            message : " Post and associated comments deleted successfully"
+        });
+    } 
+    catch(err){
+        return res.status(500).json({
+            message : " Internal server errror",
+        })
+    }
+    
+
 }
